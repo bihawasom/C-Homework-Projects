5b9391a1ef525f78d6c343da7646c879f1a79d1a
Bih Awasom <115504292+bihawasom@users.noreply.github.com>
2022-10-14 11:29:42 -0500

    Update fishbomb.cpp

--- Pointers Practice/fishbomb.cpp
+++ Pointers Practice/fishbomb.cpp
@@ -1,6 +1,5 @@
 /*
 Name: Bih Awasom
-Section: 102
 */
 #include <iostream>
 
@@ -84,50 +83,3 @@ int main(){
     
     return(0);
 }
-
-/* 
-Plan:
-one dynamite stick -> explodes vertically and horizontally, so all of the row (i) and column (j)
-0. get number of test cases X
-    cin >> testCase;
-    while loop
-
-1. inputs height and width of data grid X
-    cin >> height >> width;
-
-2. dynamically allocate array with height and width X
-    int **dataGrid;
-    dataGrid = new int * [height];
-    for(int i = 0; i < height; i++){
-        dataGrid[i] = new int[width];
-    }
-
-3. find where you can get the most fish
-    search thru each column until you find max col, save what index it is
-        int maxCol, fishCol = 0;
-        for(int row=0; row< height; row++){
-            for(int col=0; col < width; col++){
-                fishCol = dataGrid[col][row] + fishCol;
-                if(fishCol > maxCol){
-                    maxCol = fishCol;
-                }
-            }  
-            cout << "Max Column: " << maxCol << endl;
-            fishCol = 0;
-        }
-
-    search thru each row until you find max row, save what index is
-4. output #test case: at (i, j) Bender catches (# of fish) fish.
-
-5. deallocate array  X
-    for(int j = 0; j < width; j++){
-        delete[] dataGrid[j];
-    }
-    delete[] dataGrid;
-
-6. repeat from step 1-5 till test cases are over
-*/
-
-// ./a.out < sampleinput.txt -> types input
-// ./a.out < sampleinput.txt > myoutput.txt -> keeps output 
-// diff myoutput.txt sampleout.txt -> shows difference between output files
